b2ef26e4292fa1069285e4e09e2819fed7e3e4ee
Hoyee86git config --global user.name Hoyee86 <azeezabdullahi264@gmail.com>
2024-02-17 15:45:47 +0100

    lesson

---
+++ script.js
@@ -0,0 +1,55 @@
+//  VARIABLES: GLOBAL AND LOCA SCOPE VARIABLE
+
+// Global Variable
+
+let i;
+
+ for(i = 0; i < 5; i++){
+     let p = 10
+     console.log(i + p);
+ }
+
+ console.log(i)
+
+
+
+// OERATOR AND EQUALIT
+
+//  Arithmetica Operators
+//  +, -, *, /, %, ==, ===, increment
+
+// MINUS (-)
+ let cost = 15
+ let profit;
+ let salesprice = 20
+
+ profit = salesprice - cost;
+
+ console.log(profit)
+
+//  plus (+), for number
+
+ let costs = 15, profits = 5, salesprices = cost + profit;
+
+ console.log(salesprices) 
+
+//  plus (+) : for strings
+
+ let firstName = 'Olatunde', surname = 'Tobi ', fullName = surname + firstName;
+
+ console.log(fullName + "" +"is a good boy")
+
+
+ let firstNames = 'Olatunde', surnames = 'Tobi ', fullNames = surname + firstName;
+
+ console.log(`${fullNames} +  is a good boy`)
+
+
+// Back Tick (``):
+
+
+let food = 'Rice'
+console.log(`what will I eat, if not ${food}`);
+
+
+
